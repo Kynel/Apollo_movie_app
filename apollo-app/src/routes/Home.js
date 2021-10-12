121a8cea86fc7bdd556bd76c05482d300bc4fa7f
kynel <kynel95@gmail.com>
2021-10-12 17:34:32 +0900

    fetch data from Apollo client with hooks

--- apollo-app/src/routes/Home.js
+++ apollo-app/src/routes/Home.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
-const Home = () => <h1>home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  if (loading) {
+    return 'loading...';
+  }
+  if (data && data.movies) {
+    return data.movies.map((movie) => <h1>{movie.id}</h1>);
+  }
+  return <h1>Home</h1>;
+};
 
 export default Home;
