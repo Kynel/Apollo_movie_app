222caa75922b39c05e9200bf0596b9509e49f777
kynel <kynel95@gmail.com>
2021-10-12 19:38:30 +0900

    implement movie app with Apollo client

--- apollo-app/src/routes/Detail.js
+++ apollo-app/src/routes/Detail.js
@@ -1,5 +1,86 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { gql } from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import styled from 'styled-components';
 
-const Detail = () => 'Detail';
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      title
+      medium_cover_image
+      language
+      rating
+      description_intro
+    }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Container = styled.div`
+  min-height: 100vh;
+  height: 100%;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 30vw;
+  height: 45vw;
+  background-color: white;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+`;
+
+const Suggestions = styled.div``;
+
+const Detail = () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: +id },
+  });
+  console.log(loading, data);
+
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? 'Loading..' : data.movie.title}</Title>
+        <Subtitle>
+          {data?.movie?.language} · {data?.movie?.rating}
+        </Subtitle>
+        <Description>{data?.movie?.description_intro}</Description>
+      </Column>
+      {!loading && <Poster bg={data?.movie?.medium_cover_image} />}
+    </Container>
+  );
+};
 
 export default Detail;
